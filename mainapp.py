34b44d1e74db6e52e33a016ceea20cfc2c9761fa
Viswas-Programs <viswas.anand@outlook.com>
2025-11-06 14:26:39 +0530

    update

--- mainapp.py
+++ mainapp.py
@@ -5,19 +5,20 @@ import streamlit
 import json
 import google.generativeai as genai
 genai.configure(api_key="AIzaSyBTcd_mND7B_WCb1lmpo5CBRXRXh19GPS0")
-print("MODELS: ", "".join(str(item) for item in genai.list_models()))
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 #dataFetcher = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key="AIzaSyBTcd_mND7B_WCb1lmpo5CBRXRXh19GPS0", convert_system_message_to_human=True)
 def returnPrompt(INPUT):
-    return f"""Extract the following data from the given data if possible
+    return f"""Extract the following data from the given data if possible. 
 Number of ACs
-Duration running of each AC
+Duration running of each AC (In Hours)
 Number of Heaters
-Duration running of each heater
-Duration of food cooked by Microwave
-Duration of food cooked by induction stoves
-Duration of water pump motors running (motors)
+Duration running of each heater (In Hours)
+Duration of food cooked by Microwave (In Hours)
+Duration of food cooked by induction stoves (In hours)
+Duration of water pump motors running (motors) (In hours)
+Number of Fans
+Number of Lights
 
 The user input is: "{INPUT}"
 
@@ -27,6 +28,7 @@ Common electricity consumption by Heaters per hour in Kilo-watt-hours (kWh)
 Common electricity consumption by Microwaves per hour in Kilo-watt-hours (kWh)
 Common electricity consumption by induction stoves in Kilo-watt-hours (kWh)
 Common electricity consumption by water pump motors per hour in Kilo-watt-hours (kWh)
+Common electricity consumption by a light in a hour in Kilo-watt-hours (kWh)
 Average electricity usage of a house per month in Kilo-watt-hours (kWh)
 
 If you can't obtain data for any of the keys mentioned above, fill in 0 for that value.
@@ -43,6 +45,10 @@ duration_induction_stove
 avg_ec_cons_induction_stove
 duration_water_pump_motors
 avg_ec_cons_water_pump_motors
+num_lights
+num_fans
+avg_ec_cons_light
+avg_ec_cons_fans
 avg_ec_cons_house
 
 Fill in datas in SI Units only unless explicitly said otherwise in the query for the respective data.
